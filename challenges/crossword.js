50ab029e49b37523f1d0b67f0f50e8daf4961f4a
lukelovekin <lukelovekin@gmail.com>
2021-08-18 09:18:02 +1000

    debugging current errors

--- challenges/crossword.js
+++ challenges/crossword.js
@@ -19,85 +19,84 @@ function crosswordPuzzle(c, words) {
   let checkInd = 0
   let startLetter
 
-
   //  loop through whole grid
   for(let x = 0; x < col; x++ ){
       for(let y = 0; y < row; y++){
 
-        // check row until found a -
-        if (c[x][y] == "-") {
-          // let firstCords = [x,y];
-
-            const vertCount = () => {
-              let newX = x
-              while(c[newX] && c[newX][y] == '-'){
-                newX++
-                wordLength++
-              }
-              let matched
-              if(startLetter){
-                matched = toCheck.filter(word => {
-                  word[0] === startLetter
-                })
-              }else{
-                matched = toCheck.filter(word => word.length === wordLength)
-              }
-              let newerX = x
-              matched[checkInd].split('').forEach(letter => {
-                c[newerX][y] = letter
-                newerX++
-              })
-              toCheck.splice(toCheck.indexOf(matched[checkInd]), 1)
-              checked.push(matched[checkInd]);
-              console.log(toCheck)
-            }
-            
-            const horiCount = () => {
-              let newY = y
-              while (c[x][newY] == '-') {
-                newY++;
+          // is word horizontal or vertical
+          const isHoriOrVertWord = () => {
+            if (c[x][y + 1] == "-") {
+              isVert = false;
+              if (c[x][y - 1] !== "+") {
+                startLetter = c[x][y - 1];
                 wordLength++;
               }
-              let matched
-
-              console.log(startLetter)
-
-              if(startLetter){
-                matched = toCheck.filter((word) => {
-                  word[0] === startLetter;
-                });
-              }else{
-                matched = toCheck.filter(word => word.length === wordLength)
+            }else if(c[x + 1][y] == "-") {
+              isVert = true;
+              if (c[x - 1] && c[x - 1][y] !== "+") {
+                startLetter = c[x - 1][y];
+                wordLength++;
               }
-              console.log(matched)
-              let newerY
-              startLetter && matched ? newerY = y - 1 : newerY = y
-              matched[checkInd].split('').forEach(letter => {
-                c[x][newerY] = letter
-                newerY++
-              })
-              toCheck.splice(toCheck.indexOf(matched[checkInd], 1));
+            }else{
+              console.log('not given')
             }
+          };
+          const vertCountAndFit = () => {
+            let newX = x
+            while(c[newX] && c[newX][y] == '-'){
+              newX++
+              wordLength++
+            }
+            // matched will be a list of words that match the spaces length and might have a start letter
+            let matched
+            if(startLetter){
+              matched = toCheck.filter(word =>word[0] === startLetter)
+            }else{
+              matched = toCheck.filter(word => word.length === wordLength)
+              // if !matched then restart which a new word in first position
+            }
+            let newerX = x
+            startLetter && matched.length > 0 ? (newerX = x - 1) : (newerX = x);
+            matched[checkInd].split('').forEach(letter => {
+              c[newerX][y] = letter
+              newerX++
+            })
+            checked.push(matched[checkInd]);
+            toCheck.splice(toCheck.indexOf(matched[checkInd]), 1);
+            console.log(matched)
+          }          
+          const horiCountAndFit = () => {
+            let newY = y
+            while (c[x][newY] == '-') {
+              newY++;
+              wordLength++;
+            }
+            let matched
+            if(startLetter){
+              matched = toCheck.filter(word => word[0] === startLetter)
+            }else{
+              matched = toCheck.filter(word => word.length === wordLength)
+            }
+            console.log(matched)
+            let newerY
+            startLetter && matched.length > 0 ? newerY = y - 1 : newerY = y
+            matched[checkInd].split('').forEach(letter => {
+              c[x][newerY] = letter
+              newerY++
+            })
+            checked.push(matched[checkInd]);
+            toCheck.splice(toCheck.indexOf(matched[checkInd]),1)
+            console.log(toCheck)
+          }
 
-          //  is vertical or horizontal word
-          if (c[x][y + 1] == "-") {
-            isVert = false
-            if(c[x][y - 1] !== "+"){
-              startLetter = c[x][y - 1];
-              wordLength++
-            }
-          }
-          if (c[x + 1][y] == "-") {
-            isVert = true
-            if (c[x - 1] && c[x - 1][y] !== "+") {
-              firstLetter = c[x - 1][y];
-              wordLength++
-            }
-          }
+        // check row until found a -
+        if (c[x][y] == "-") {
+          isHoriOrVertWord()
+
           // if theres a - both then its a join
           if (c[x + 1][y] == "-" && c[x][y + 1] == "-") console.log("JOIN")
           
-          isVert ? vertCount() : horiCount();
+          isVert ? vertCountAndFit() : horiCountAndFit();
           // wordLengths.push(wordLength)
         }
         wordLength = 0;
@@ -106,8 +105,6 @@ function crosswordPuzzle(c, words) {
 
   console.log(c)
   // 6, 5, 7, 6
-
-
 }
 
 // const crossword = [
@@ -128,14 +125,14 @@ const crossword = [
   ["+","-","+","+","+","+","+","+","+","+"],
   ["+","-","+","+","+","+","+","+","+","+"],
   ["+","-","-","-","-","-","+","+","+","+"],
-  ["+","-","+","+","+","+","+","+","+","+"],
-  ["+","-","+","+","+","+","+","+","+","+"],
-  ["+","+","+","+","+","+","+","+","+","+"],
-  ["+","+","+","+","+","+","+","+","+","+"],
-  ["+","+","+","+","+","+","+","+","+","+"],
-  ["+","+","+","+","+","+","+","+","+","+"]
+  ["+","-","+","+","+","-","+","+","+","+"],
+  ["+","-","+","+","+","-","+","+","+","+"],
+  ["+","+","+","+","+","-","+","+","+","+"],
+  ["+","+","-","-","-","-","-","-","+","+"],
+  ["+","+","+","+","+","-","+","+","+","+"],
+  ["+","+","+","+","+","-","+","+","+","+"]
 ]
 
-const words = ["LONDON","DELHI",/*"ICELAND","ANKARA"*/]
+const words = ["LONDON","DELHI","ICELAND", "ANKARA"]
 
 crosswordPuzzle(crossword, words)
